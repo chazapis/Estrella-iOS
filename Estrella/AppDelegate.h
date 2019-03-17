bc07c6963297d00e1f0f5c4551d520844e80b394
Antony Chazapis <chazapis@gmail.com>
2019-03-17 12:04:56 +0200

    Initial Commit

---
+++ Estrella/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Estrella
+//
+//  Created by Antony Chazapis on 17/03/2019.
+//  Copyright © 2019 Koomasi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Estrella/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Estrella
+//
+//  Created by Antony Chazapis on 17/03/2019.
+//  Copyright © 2019 Koomasi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
